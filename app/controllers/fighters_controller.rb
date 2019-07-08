b88f2f2524557cf7114eacf3d9611d54abcf0336
Jake <jakelovitz@gmail.com>
2019-07-08 19:02:59 -0400

    finished functionality to handle POST request

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -2,12 +2,33 @@ class FightersController < ApplicationController
 
     def index
         render json: Fighter.all 
-    end 
+    end
+
+
+
+    def create
+        @fighter = Fighter.find_or_create_by(fighter_params)
+        @moves = Move.find_or_initialize_by(moves_params)
+        @moves.fighter = @fighter
+        @moves.save
+        render json: @fighter
+    end
+
 
     def show
         render json: Fighter.find(params[:id])
-    end 
+    end
 
+
+    private
+
+    def fighter_params
+        params.permit(:name, :img_url)
+    end
+
+    def moves_params
+        params.require(:moves).permit(:name1, :img_url1, :dp1, :pp1, :name2, :img_url2, :dp2, :pp2, :name3, :img_url3, :dp3, :pp3, :name4, :img_url4, :dp4, :pp4)
+    end
 
     
 
