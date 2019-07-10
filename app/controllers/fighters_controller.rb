e3699c1e3f3c54c439e99713a3287ab417b5cef6
Jake <jakelovitz@gmail.com>
2019-07-09 23:05:09 -0400

    successfully got move objects to persist

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -7,11 +7,18 @@ class FightersController < ApplicationController
 
 
     def create
-       
-        @fighter = Fighter.create(:name => params["fighterName"], :img_url => params["fighterImage"], :hp => params["health"])
-       
-        @move = Move.create(:name => params["name"], :image => params["image"], :damage => params["damage"], :power => params["power"], :fighter_id => @fighter.id)
 
+        @fighter = Fighter.create(:name => params["fighterName"], :img_url => params["fighterImage"], :hp => params["health"])
+        
+        @move_one = Move.create(:name => params["moveOne"]["name"], :image => params["moveOne"]["image"], :damage => params["moveOne"]["damage"], :power => params["moveOne"]["power"], :fighter_id => @fighter.id)
+
+        @move_two = Move.create(:name => params["moveTwo"]["name"], :image => params["moveTwo"]["image"], :damage => params["moveTwo"]["damage"], :power => params["moveTwo"]["power"], :fighter_id => @fighter.id)
+
+        @move_three = Move.create(:name => params["moveThree"]["name"], :image => params["moveThree"]["image"], :damage => params["moveThree"]["damage"], :power => params["moveThree"]["power"], :fighter_id => @fighter.id)
+
+        @move_four = Move.create(:name => params["moveFour"]["name"], :image => params["moveFour"]["image"], :damage => params["moveFour"]["damage"], :power => params["moveFour"]["power"], :fighter_id => @fighter.id)
+        
+        
         
 
         render json: @fighter
